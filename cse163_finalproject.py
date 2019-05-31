0c478b7a67a6516f89477cac80a214215e7d290e
kimyoungwon <42983671+kimyoungwon@users.noreply.github.com>
2019-05-31 12:48:16 -0700

    Update cse163_finalproject.py

---
+++ cse163_finalproject.py
@@ -0,0 +1,40 @@
+#
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error
+
+################################################################################
+# Step 1: Read and clean .......data set.
+################################################################################
+
+def read_csv(path):
+    '''
+    Reads the CSV file at path, and returns a dataframe.
+    '''
+    return 
+
+def means_of_variables(dt, name_of_variable):
+    return
+
+def get_pearsonr(dt,names_of_variables):
+    return
+
+def bar_grapgh(dt, variable):
+    return
+
+def scatter_plot(dt, variable1, variable2):
+    return
+
+################################################################################
+# Main Function
+################################################################################
+
+def main():
+
+if __name__ == "__main__":
+    main()
